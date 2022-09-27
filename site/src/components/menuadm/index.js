c9282df06655897abffbfbf6ab30df1af7922330
Ashley da Silva Aquino <ashleysilvaaquino@gmail.com>
2022-09-27 16:35:05 -0300

    teste

---
+++ site/src/components/menuadm/index.js
@@ -0,0 +1,46 @@
+import './index.scss'
+import Home from '../../assets/images/home.png'
+import Cadastrar from '../../assets/images/cadastrar.png'
+import Gerenciar from '../../assets/images/gerenciar.png'
+import Pedidos from '../../assets/images/pedidos.png'
+import Sair from '../../assets/images/sair.png'
+
+
+export default function Menu(){
+
+   return(
+      <nav className="menu-total">
+      <div>
+         <div className='menu-items'>
+             <div>
+              <img src={Home}  />
+              <div>Home</div>
+            </div>
+
+            <div>
+               <img src={Cadastrar} />
+               <div>Cadastrar</div>
+            </div>
+            <div>
+               <img src={Gerenciar} />
+               <div>Gerenciar</div>
+            </div>
+            <div>
+               <img src={Pedidos}/>
+               <div>Pedidos</div>
+            </div>
+  
+  
+      </div>
+  </div>
+  
+  <div className='menu-items'>
+      <div>
+          <img src={Sair} />
+          <div>Sair</div>
+      </div>
+  </div>
+  </nav>
+   );
+   
+}
