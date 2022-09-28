f5815f7518f60828069ccb8d7676b779efd60305
Guilherme <pepe.ks.rx.1@gmail.com>
2022-09-28 14:45:17 -0300

    teste card do livro

---
+++ site/src/pages/consultarLivro/index.js
@@ -0,0 +1,12 @@
+import CardLivro from "../../components/cardGerenciarLivro";
+
+export default function ConsultarLivro() {
+
+    return(
+        <main>
+            <div>
+
+            </div>
+        </main>
+    );
+}
