aab0355cb9d386dba9aa08d3c4e41107a7da0f87
anacarolinaa <anacarolinaribeirosilca2580@gmail.com>
2022-09-30 14:42:08 -0300

    paginacadastrarcliente2 cpmpletassa

--- site/src/pages/cadastrarcliente2/index.js
+++ site/src/pages/cadastrarcliente2/index.js
@@ -4,30 +4,40 @@ import logofrei from '../../assets/images/logofrei.png'
 export default function CadastrarCliente() {
     return (
 
-        <main className='nomee'>
+        <main>
             <div className='logo'>
                 <img src={logofrei} alt="" className='loguinho' />
             </div>
 
 
-        <div className='main'>
+        <div className='nomee'>
             <section className='section1'>
 
+               <div className = "opa">
+                    <h1>OPA, VOCÊ ESTÁ <span>QUASE LÁ </span></h1>
+                </div>
+
+
                 <div className="dados">
-                    <h1>Opa, você está <span> quase lá </span></h1>
+               
+
+                    <label className= "dado">Estado</label> 
+                    <select>
+                            <option selected disabled hidden>Ex: São Paulo</option>
+
+                    </select>
+                
+        
+
+                    <label className='dado'>CEP:</label>
+                    <input  className='input-cep' placeholder='Ex: 22222-222' />
 
 
-                //select estado
+                    <p className="dado">Número:</p>
+                    <input  className='input-numero'  placeholder='Ex: 100' />
 
-                    <p className='dado'>CEP:</p>
-                    <input type="number" className='input-cep' />
-
-
-                    <p className="dado">Número</p>
-                    <input type="number" className='input-numero' />
-
-                    <p className="dado">Celular</p>
-                    <input type="number" className='input-numero' />
+                    <p className="dado">Celular:</p>
+                    <input className='input-numero' placeholder='11 9999-1010'/>
 
                 </div>
 
