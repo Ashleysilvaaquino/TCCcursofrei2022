1c7b22b7bfcfc4f270f1a70a2992624747c6e1b0
anacarolinaa <anacarolinaribeirosilca2580@gmail.com>
2022-10-05 15:15:39 -0300

    pagina de pagamento cartao

---
+++ site/src/pages/paginadepagamentocartao/index.js
@@ -0,0 +1,29 @@
+import './index.scss';
+
+function pagarCartao() {
+
+    return (
+
+        <main>
+            <div className='logo'>
+                //imagem
+            </div>
+
+            <div className='dados'>
+
+            <label></label>
+
+            </div>
+            <div className='codigo'>
+
+            </div>
+
+        </main>
+
+    )
+
+
+
+
+}
+export default pagarCartao;
