9cde51059c26019b1f81711d8a3b36976fa64c82
Isabella <isacosta90a@gmail.com>
2022-10-26 21:25:20 -0300

    jsx pag detalhe livro

--- site/src/components/livrodetalhe/index.js
+++ site/src/components/livrodetalhe/index.js
@@ -7,26 +7,8 @@ import Close from '../../assets/images/Close.png';
 
 
 
-import Storage from 'local-storage'
-
-function LivroDetalhe(props){
-    const [livros, setLivros] = useState([]);
-
-    function adicionarAoCarrinho(){
-        let carrinho = [];
-        if(Storage('carrinho')){
-            carrinho = Storage('carrinho');
-        }
-        if(!carrinho.find(livros.id === livros.id)){
-            carrinho.push({
-                id: livros.id,
-                qtd:1
-            })
-         Storage('carrinho, carrinho');    
-        } 
-    }
+function LivroDetalhe(){
     
-
     return(
      <div className='pag-detalhe'>
         <img src={Favorito} className='img-favorito'/> 
@@ -36,24 +18,24 @@ function LivroDetalhe(props){
             <h3>AUTOR</h3>
         </div>
         <div className='cont-nome-autor'>
-            <p>{props.livros.nome}</p>
-            <p>{props.livros.autor}</p>
+            <p>Mais esperto que o diabo</p>
+            <p>Napoleon Hill</p>
         </div>
         <div className='genero-paginas'>
             <h3>GENÊRO</h3>
             <h3>PÁGINAS</h3>
         </div>
         <div className='cont-genero-pag'>
-            <p>{props.livros.paginas}</p>
-            <p>{props.livros.genero}</p>
+            <p>Autoajuda</p>
+            <p>208</p>
         </div>
         <div className='div-desc'>
             <h3>DESCRIÇÃO</h3>
-            <p>{props.livros.descricao}</p>
+            <p>O livro Mais Esperto que o Diabo foi escrito como uma sessão de perguntas e respostas entre Napoleon Hill e o próprio Diabo, que afirma influenciar 98% da população. Ele afirma fazer isso através de 6 grandes medos que a maioria das pessoas tem, que são: Medo da pobreza. Medo da crítica.</p>
         </div>
         
         <div className='buttons-pg-detalhe'>
-            <button onClick={adicionarAoCarrinho} className='button-add'>ADICIONAR AO CARRINHO</button>
+            <button className='button-add'>ADICIONAR AO CARRINHO</button>
             <button className='button-comprar'>COMPRAR</button>
         </div>
 
