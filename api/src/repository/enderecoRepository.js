10fa876e09844ac15e17fd93e4f3103653c5827d
Ashley da Silva Aquino <ashleysilvaaquino@gmail.com>
2022-11-08 16:34:36 -0300

    endereco alteracao

--- api/src/repository/enderecoRepository.js
+++ api/src/repository/enderecoRepository.js
@@ -2,28 +2,28 @@ import { con } from "./connection.js";
 
 //login usuario
 export async function listarEndereco(idContaUsuario) {
-    const comando = ` select
-    ds_referencia		referencia,
-    nr_cep				cep,
-      nr_residencia		residencia,
-      ds_estado			estado,
-      ds_cidade			cidade,
+  const comando = ` select
+      id_conta_usuario  id,
+      ds_referencia		referencia,
+      nr_cep				  cep,
+      ds_estado		   	estado,
+      ds_cidade			  cidade,
       ds_logradouro		logradouro,
-      ds_complemento		complemento,
+      ds_complemento	complemento,
       ds_bairro			bairro
       
   from tb_usuario_endereco
   where id_conta_usuario = ?;`
- 
-    const registro = await con.query(comando, [idContaUsuario]);
-    return registro;
- }
+
+  const registro = await con.query(comando, [idContaUsuario]);
+  return registro;
+}
 
 
- export async function salvarEndereco(idContaUsuario, endereco ) {
-   const comando = `insert into tb_usuario_endereco(ds_referencia, nr_cep, nr_residencia, ds_estado, ds_cidade, ds_logradouro, ds_complemento, ds_bairro)
+export async function salvarEndereco(idContaUsuario, endereco) {
+  const comando = `insert into tb_usuario_endereco(ds_referencia, nr_cep, nr_residencia, ds_estado, ds_cidade, ds_logradouro, ds_complemento, ds_bairro)
                                              values(?,?,?,?,?,?,?,?)`
 
-   const [info] = await con.query(comando, [idContaUsuario, endereco.cep, endereco.residencia, endereco.estado, endereco.cidade, endereco.logradouro, endereco.complemento, endereco.bairro ]);
-   return info.insertId;
+  const [info] = await con.query(comando, [idContaUsuario, endereco.referencia, endereco.cep, endereco.residencia, endereco.estado, endereco.cidade, endereco.logradouro, endereco.complemento, endereco.bairro]);
+  return info.insertId;
 }
