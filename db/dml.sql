1e63095277bc5daf947d1049b48ce97af43f202f
ashley <ashleysilvaquino@gmail.com>
2022-11-21 15:21:19 -0300

    inserts

--- db/dml.sql
+++ db/dml.sql
@@ -44,8 +44,8 @@ select 	tb_conta_usuario.id_conta_usuario,
         where ds_email = 'ashleysilvaaquino2@gmail.com'
   and ds_senha = '21918570173';
 
-insert into tb_genero(nm_genero)
-   values('Romance');
+   insert into tb_genero(nm_genero)
+      values('Romance');
 
 select * from tb_genero;
 
@@ -75,28 +75,28 @@ insert into tb_livro(nm_livro, nm_autor,vl_preco,ds_livro,nr_paginas, id_genero)
    values('É assim que começa', 'Collen Hoover', '40.00', 'É Assim que Começa “Lily e o seu ex-marido, Ryle, acabaram de se adaptar numa situação de compartilhamento de guarda quando, de repente, ela se reencontra com seu primeiro amor, Atlas. Depois de quase dois anos separados, o tempo finalmente parece estar ao favor de ambos, e Lily imediatamente aceita o convite para um encontro.', '390', 1);
 
 insert into tb_livro(nm_livro, nm_autor,vl_preco,ds_livro,nr_paginas, id_genero)
-   values('É assim que acaba', 'Collen Hoover', '25.00', ' É assim que acaba discute temas como violência doméstica e abuso psicológico de forma sensível e direta. Em É assim que acaba, Colleen Hoover nos apresenta Lily, uma jovem que se mudou de uma cidadezinha do Maine para Boston, se formou em marketing e abriu a própria floricultura.', '386', 1);
+   values('É assim que acaba', 'Collen Hoover', '25.00', ' É assim que acaba discute temas como violência doméstica e abuso psicológico de forma sensível e direta. Em É assim que acaba, Colleen Hoover nos apresenta Lily.', '386', 5);
 
 
  insert into tb_livro(nm_livro, nm_autor,vl_preco,ds_livro,nr_paginas, id_genero)
-   values('Hertstopper', 'Alice Oceman', '25.00', 'conta a história de dois jovens que se conhecem num grupo de estudo e começam a desenvolver uma relação de amizade que se tornará muito mais do que aquilo que inicialmente estavam a contar. Charlie, é um excelente aluno, que frequenta uma escola só para rapazes e que toda a gente sabe que é homossexual.', '291',1);
+   values('Hertstopper', 'Alice Oceman', '25.00', 'Conta a história de dois jovens que se conhecem num grupo de estudo e começam a desenvolver uma relação de amizade que se tornará muito mais do que aquilo que inicialmente estavam a contar.', '291',5);
 
 insert into tb_livro(nm_livro, nm_autor,vl_preco,ds_livro,nr_paginas, id_genero)
-   values('Até o verão Terminar', 'Collen Hoover', '30.00', 'Até o Verão Terminar (título em inglês "Heart Bones") é um desses livros que você não percebe o tempo passar. Estive envolvida com a história da Beyah Grim e o Samson do início ao fim. Beyah é uma garota negligenciada pelos pais, que teve que lutar sozinha, se sujeitando a situações humilhantes e traumáticas para poder existir.', '336', 1);
+   values('Até o verão Terminar', 'Collen Hoover', '30.00', 'Até o Verão Terminar (título em inglês "Heart Bones") é um desses livros que você não percebe o tempo passar. Estive envolvida com a história da Beyah Grim e o Samson do início ao fim.', '336', 5);
 
 insert into tb_livro(nm_livro, nm_autor,vl_preco,ds_livro,nr_paginas, id_genero)
-   values('A maldição do mar', 'Shea Ernshaw', '15.00', 'A Maldição do Mar é o livro de estreia de Shea Ernshaw - publicado aqui no Brasil pela Galera Record. Na história, todo o verão a cidade de Sparrow passa pela temporadas das Swan, quando três irmãs - assassinadas ao serem acusadas de bruxaria - tomam o corpo de três garotas para se vingar da cidade atraindo e afogando garotos nas docas.', '320', 1);
-
-
-insert into tb_livro(nm_livro, nm_autor,vl_preco,ds_livro,nr_paginas, id_genero)
-   values('Antes que as luzes se apaguem', 'Jay Asher', '15.00', 'Antes Que As Luzes Se Apaguem é um livro de 2016 que conta a história da adolescente Sierra. Ela vive no estado gelado do Oregon, onde tem seus amigos, sua escola, toda a sua vida. Porém, todo natal ela e a família passam um mês na Califórnia onde possuem uma fazenda que vende pinheiros para o natal.', '256', 1);
+   values('A maldição do mar', 'Shea Ernshaw', '15.00', 'A Maldição do Mar é o livro de estreia de Shea Ernshaw - publicado aqui no Brasil pela Galera Record. Na história, todo o verão a cidade de Sparrow passa pela temporadas das Swan, quando três irmãs.', '320', 1);
 
 
 insert into tb_livro(nm_livro, nm_autor,vl_preco,ds_livro,nr_paginas, id_genero)
-   values('Eu e esse meu coração', 'C. C. Hunter', '25.00', 'Eu e esse meu coração possui uma sinopse bem explicativa. Leah é uma jovem de 17 anos de idade, que está vivendo um dia de cada vez. Após um vírus ter causado miocardite, Leah sobrevive graças a um coração artificial, uma "bomba" que está acoplada a uma mochila e que mantêm os seus órgãos funcionando.', '424', 1);
+   values('Antes que as luzes se apaguem', 'Jay Asher', '15.00', 'Antes Que As Luzes Se Apaguem é um livro de 2016 que conta a história da adolescente Sierra. Ela vive no estado gelado do Oregon, onde tem seus amigos, sua escola, toda a sua vida.', '256', 1);
+
 
 insert into tb_livro(nm_livro, nm_autor,vl_preco,ds_livro,nr_paginas, id_genero)
-   values('Ainda sou eu', 'Lou Clark', '26.00', 'Resumo do livro Ainda sou eu – Jojo Moyes A personagem principal do livro, Lou Clark, é uma mulher jovem, cativante e muito bem humorada. Ela vive cercada por novas aventuras, desafios e difíceis decisões.', '400', 1);
+   values('Eu e esse meu coração', 'C. C. Hunter', '25.00', 'Eu e esse meu coração possui uma sinopse bem explicativa. Leah é uma jovem de 17 anos de idade, que está vivendo um dia de cada vez. Após um vírus ter causado miocardite.', '424', 5);
+
+insert into tb_livro(nm_livro, nm_autor,vl_preco,ds_livro,nr_paginas, id_genero)
+   values('Ainda sou eu', 'Lou Clark', '26.00', 'Resumo do livro Ainda sou eu – Jojo Moyes A personagem principal do livro, Lou Clark, é uma mulher jovem, cativante e muito bem humorada. Ela vive cercada por novas aventuras, desafios e difíceis decisões.', '400', 5);
 
 
 
@@ -120,7 +120,7 @@ select * from tb_livro;
 
     
 insert into tb_adm_login(ds_email, ds_senha)
-   values('admlivrariamonte@gmail.com', 'senha1234' );
+   values('adm@gmail.com', '1234' );
    
    
 select * from tb_adm_login;
